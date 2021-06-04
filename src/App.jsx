9797512859a6ae47b25ef07e38f9dce9fadfc93a
aragrevo <eduver_san@hotmail.com>
2021-06-03 19:42:55 -0500

    text-slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,32 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+import { Hero } from './components/Hero';
 // import
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className='App-link'
-            href='https://vitejs.dev/guide/features.html'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='container'>
+      <div className='navbar'>
+        <h3>Coding</h3>
+        <nav>
+          <ul>
+            <li>
+              <a href=''>Home</a>
+            </li>
+            <li>
+              <a href=''>About</a>
+            </li>
+            <li>
+              <a href=''>Home</a>
+            </li>
+            <li>
+              <a href=''>Home</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Hero />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Hero = () => {
+  return (
+    <div className='content'>
+      <div className='text'>
+        Become a
+        <div className='slider'>
+          <div>programmer</div>
+          <div>web developer</div>
+          <div>angular</div>
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut, ratione.
+      </p>
+    </div>
+  );
+};
